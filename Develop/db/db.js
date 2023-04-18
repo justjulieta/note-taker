27f782075b8701b23b62c86d01e4dc5a94f9326f
Julieta Ramirez Solis Nguyen <julieta.rammirez27@gmail.com>
2023-04-17 19:10:19 -0700

    Changes to be committed:

--- Develop/db/db.js
+++ Develop/db/db.js
@@ -2,17 +2,48 @@ const util = require('util');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-const readData = 'util.promsify(fs.readfile)';
-const writeData = util.promisify(fs.writefile);
-
+const readData = util.promisify(fs.readFile);
+const writeData = util.promisify(fs.writeFile);
 class Save {
-    write (note) {
-        return data('db/db.json', JSON.stringify(note));
+    write(note) {
+        return writeData('db/db.json', JSON.stringify(note));
     }
+
     read() {
-        return data('db/db.json', 'utf8');
+        return readData('db/db.json', 'utf8');
     }
+
     retrieveNotes() {
-        return data('db/db.json,')
+        return this.read().then(notes => {
+            let dataNotes;
+            try {
+                dataNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                dataNotes = [];
+            }
+            return dataNotes;
+        });
+    }
+
+    addNote(note) {
+        const { title, text } = note;
+        if (!title || !text) {
+            throw new Error('Enter in a value');
+        }
+
+        const newNote = { title, text, id: uuidv4() };
+
+        return this.retrieveNotes()
+            .then(notes => [...notes, newNote])
+            .then(updatedNotes => this.write(updatedNotes))
+            .then(() => newNote);
+    }
+
+    deleteNote(id) {
+        return this.retrieveNotes()
+            .then(notes => notes.filter(note => note.id !== id))
+            .then(filteredNotes => this.write(filteredNotes));
     }
 }
+
+module.exports = new Save();
