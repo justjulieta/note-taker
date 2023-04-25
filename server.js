cf83e500dda88f77e01a942e8cc634f28195fe00
Julieta Ramirez Solis Nguyen <julieta.rammirez27@gmail.com>
2023-04-24 20:25:57 -0700

    Changes to be committed:

--- server.js
+++ server.js
@@ -1,23 +1,46 @@
-// Import modules
-const express = require("express"); 
+const express = require("express");
 const path = require("path");
-const api = require("../routes/index.js");
-
-// Port used for Heroku
-const PORT = process.env.PORT || 3001;
-
-// Initialize express
+const fs = require("fs");
+const notes = require("./db/db.json");
+const uuid = require('uuid');
 const app = express();
 
-// Middleware for parsing JSON + URL incoded data
+var PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", api);
-
-// Authroization to make it public
 app.use(express.static("public"));
 
-// Starts listening
-app.listen(PORT, () => {
-    console.log('Opened on ${PORT}')
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const dltNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id); 
+    fs.writeFileSync("./db/db.json", JSON.stringify(dltNote));
+    res.json(dltNote);
 })
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.get("/api/notes", (req, res) => 
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+);
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4(); 
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT, function () {
+    console.log(`Example app listening at http://localhost:${PORT} 🚀`)
+});
